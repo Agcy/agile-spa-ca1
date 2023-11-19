a842a7d8294eeca2def1ea110fdfe5ad971b9c74
Agcy Liang· <liang765407782@gmail.com>
2023-11-19 00:26:59 +0000

    refactoring movie detail page complete

--- src/components/movie/templateMoviePage/index.js
+++ src/components/movie/templateMoviePage/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { getMovieImages } from "../../../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from '../../spinner'
@@ -13,6 +15,8 @@ const TemplateMoviePage = ({ movie, children }) => {
     getMovieImages
   );
 
+    const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
   if (isLoading) {
     return <Spinner />;
   }
@@ -22,37 +26,60 @@ const TemplateMoviePage = ({ movie, children }) => {
   }
   const images = data.posters
 
+    const handlePrev = () => {
+        setCurrentImgIndex((prevIndex) =>
+            prevIndex > 0 ? prevIndex - 1 : images.length - 1
+        );
+    };
+
+    const handleNext = () => {
+        setCurrentImgIndex((prevIndex) =>
+            prevIndex < images.length - 1 ? prevIndex + 1 : 0
+        );
+    };
+
   return (
-    <>
-      <MovieHeader movie={movie} />
+      <>
+        <MovieHeader movie={movie} />
 
-      <Grid container spacing={5} sx={{ padding: "15px" }}>
-        <Grid item xs={3}>
-          <div sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-          }}>
-            <ImageList
-                cols={1}>
-                {images.map((image) => (
-                    <ImageListItem key={image.file_path} cols={1}>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.poster_path}
-                    />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-          </div>
-        </Grid>
+        <Grid container spacing={5} sx={{ padding: "15px" }}>
+          <Grid item xs={3}>
+            <Paper sx = {{
+              position: 'relative',
+              width: '100%',
+              height: 500,
+              backgroundImage: `url(https://image.tmdb.org/t/p/w500/${images[currentImgIndex].file_path})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}>
+              <IconButton
+                  sx = {{
+                    position: 'absolute',
+                    left: 0,
+                    top: '50%'
+                  }}
+                  onClick={handlePrev}
+              >
+                <ArrowBackIosIcon/>
+              </IconButton>
+              <IconButton
+                  sx={{
+                    position: 'absolute',
+                    right: 0,
+                    top: '50%'
+                  }}
+                  onClick={handleNext}
+              >
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
 
-        <Grid item xs={9}>
-          {children}
+          <Grid item xs={9}>
+            {children}
+          </Grid>
         </Grid>
-      </Grid>
-    </>
+      </>
   );
 };
-
 export default TemplateMoviePage;
