12abd8a6fd3a3e3250aec59b5f5f99ace8814432
Agcy Liang· <liang765407782@gmail.com>
2023-11-21 14:31:06 +0000

    test actor detail and credit render complete

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('goToActorPage', (actorId) => {
     cy.visit(`/actors/${actorId}`)
 })
+
+Cypress.Commands.add('goToMoviePage', (movieId) => {
+    cy.visit(`/movies/${movieId}`)
+})
